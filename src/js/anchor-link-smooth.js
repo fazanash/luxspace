c2f698af20f94144f9e168c4cffd0a96e245a206
fazanash <fazanash94@gmail.com>
2021-03-11 15:45:07 +0700

    menambahkan file js untuk anchor

--- src/js/anchor-link-smooth.js
+++ src/js/anchor-link-smooth.js
@@ -1,6 +1,10 @@
 const smoothScrollAnchor = decument.querySelectorAll("a[href^='#']")
 
 for (let index = 0; index < smoothScrollAnchor.length; index++) {
-    const element = smoothScrollAnchor[index];
+    const e = smoothScrollAnchor[index];
     
+    e.addEventListener("click", function(){
+        e.preventDefault();
+        if(decument.getElementById(this.get.attribute("href").replace("#", "")))
+    })
 }
